03222bc917f12b560f22187f0b051a6fd21e2418
fluke006 <johongirovdiyorbek08@gmail.com>
2021-11-20 09:47:25 +0500

    add checkout

--- src/AuthenticatedApp.jsx
+++ src/AuthenticatedApp.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
-import UserPage from './Pages/User/User';
+// import UserPage from './Pages/User/User';
+import Bookmarks from './Pages/Bookmarks/Bookmarks';
 
 function AuthenticatedApp() {
 	return (
 		<>
-			<UserPage />
+			{/* <UserPage /> */}
+			<Bookmarks />
 		</>
 	);
 }
