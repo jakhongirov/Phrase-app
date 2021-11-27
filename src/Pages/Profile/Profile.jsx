86d6112fd666f68de97cb18bef7fcba0bcbfa94f
Islombek-FD <islombekqurbonovfd@gmail.com>
2021-11-28 01:17:59 +0300

    Initial commit

---
+++ src/Pages/Profile/Profile.jsx
@@ -0,0 +1,16 @@
+function Profile() {
+	return (
+		<>
+			<h1>Diyrbek Qalay bovotti ishlar</h1>
+
+			<p>
+				Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+				Quidem, accusantium? At adipisci, iusto, iste nesciunt natus,
+				quisquam optio quam illum excepturi ut delectus tenetur minus
+				consequuntur! Asperiores eveniet facilis mollitia?
+			</p>
+		</>
+	);
+}
+
+export default Profile;
