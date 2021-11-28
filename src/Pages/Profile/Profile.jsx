46004da12c1b56090763325dd459cdc5b64357b0
Islombek-FD <islombekqurbonovfd@gmail.com>
2021-11-28 11:39:32 +0300

    Add new profile page to project

--- src/Pages/Profile/Profile.jsx
+++ src/Pages/Profile/Profile.jsx
@@ -1,14 +1,9 @@
+import ProfileContent from '../../Components/ProfileContent/ProfileContent.jsx';
+
 function Profile() {
 	return (
 		<>
-			<h1>Diyrbek Qalay bovotti ishlar</h1>
-
-			<p>
-				Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-				Quidem, accusantium? At adipisci, iusto, iste nesciunt natus,
-				quisquam optio quam illum excepturi ut delectus tenetur minus
-				consequuntur! Asperiores eveniet facilis mollitia?
-			</p>
+			<ProfileContent />
 		</>
 	);
 }
