cca71d9cb9c70fa0a295498d9d26ada21d8bfdf8
Nurkhayot <nurxayotmansurov@gmail.com>
2021-11-30 08:28:18 +0500

    created modal

--- src/Components/Modal/Modal.jsx
+++ src/Components/Modal/Modal.jsx
@@ -1,11 +1,27 @@
 import React from "react";
+import "./Modal.scss";
 
-function Modal() {
+function Modal({ show, onClose }) {
+  if (!show) {
+    return null;
+  }
   return (
-    <div className="modal">
-      <div className="modal-content">
+    <div className="modal" onClick={onClose}>
+      <div className="modal-content" onClick={(e) => e.stopPropagation()}>
         <div className="modal-header">
           <h4 className="modal-title">Are you sure delete this user?</h4>
+        </div>
+
+        <div className="modal-body">
+          Lorem Ipsum is simply dummy text of the printing and typesetting
+          industry. Lorem
+        </div>
+
+        <div className="modal-footer">
+          <button className="modal-btn modal-btn--cancel" onClick={onClose}>
+            Cancel
+          </button>
+          <button className="modal-btn modal-btn--delete">Delete</button>
         </div>
       </div>
     </div>
--- src/Components/UserBtns/UserBtns.jsx
+++ src/Components/UserBtns/UserBtns.jsx
@@ -1,10 +1,22 @@
 import React from "react";
 
 import "./UserBtns.scss";
+import Modal from "../Modal/Modal";
 
 function UserBtnRed() {
-  return <div className="btn btn--red">Delete User</div>;
+  const [show, setShow] = React.useState(false);
+
+  return (
+    <>
+      <div className="btn btn--red" onClick={() => setShow(true)}>
+        Delete User
+      </div>
+
+      <Modal onClose={() => setShow(false)} show={show} />
+    </>
+  );
 }
+
 function UserBtnBlue() {
   return <div className="btn btn--blue">Manage User</div>;
 }
