817b60bd49ffd8883106909f221ce85a921d4113
jakhongirov <87989795+jakhongirov@users.noreply.github.com>
2021-12-12 21:18:59 +0500

    clean main

--- src/Components/Lib/Statistics.jsx
+++
@@ -1,7 +0,0 @@
-function Statistics () {
-    return (
-        <svg width="20" height="20" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M17 10h-7V3c0-.6-.4-1-1-1-5 0-9 4-9 9s4 9 9 9 9-4 9-9c0-.6-.4-1-1-1Zm-7 7.9c-3.8.6-7.4-2.1-7.9-5.9C1.5 8.2 4.2 4.6 8 4.1V11c0 .6.4 1 1 1h6.9c-.4 3.1-2.8 5.5-5.9 5.9ZM13 0c-.6 0-1 .4-1 1v6c0 .6.4 1 1 1h6c.6 0 1-.4 1-1 0-3.9-3.1-7-7-7Zm1 6V2.1c2 .4 3.5 1.9 3.9 3.9H14Z" fill="#5F6471"/></svg>
-    )
-}
-
-export default Statistics;
--- src/Components/Lib/plus.jsx
+++
@@ -1,7 +0,0 @@
-function Plus () {
-    return (
-        <svg width="20" height="20" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M18.75 8.75h-7.5v-7.5a1.25 1.25 0 0 0-2.5 0v7.5h-7.5a1.25 1.25 0 0 0 0 2.5h7.5v7.5a1.25 1.25 0 0 0 2.5 0v-7.5h7.5a1.25 1.25 0 0 0 0-2.5Z" fill="#fff"/></svg>
-    )
-}
-
-export default Plus;
